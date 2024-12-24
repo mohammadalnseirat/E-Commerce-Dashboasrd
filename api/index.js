df0a462ffa330e02cb40b299ba212a62aa763278
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-12-24 19:18:33 +0400

    Create User Model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -10,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-
+//? Routes:
+app.use("/api/v1/auth", authRoutes);
 //? listen to the port:
 app.listen(PORT, () => {
   connectDB();
---
+++ api/models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
+//! hashing the password:
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+    next();
+  } catch (error) {
+    console.log("Error hashing password:", error.message);
+    next(error);
+  }
+});
+
+//? Matching the password:
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcryptjs.compare(enteredPassword, this.password);
+};
+const User = mongoose.model("User", userSchema);
+
+export default User;
